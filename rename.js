5b8fc2a522af5dd366ba53aea78cad824bd87179
Vamshi <122205613+bvamshi7612@users.noreply.github.com>
2023-03-27 20:27:30 +0530

    Add files via upload

---
+++ rename.js
@@ -0,0 +1,10 @@
+const nav = document.getElementById("navbar") ;
+const close = document.getElementById("close") ;
+function closesidenav(){
+    nav.classList.remove("active")
+
+}
+
+function opensidenav(){
+   nav.classList.add("active")
+   }
